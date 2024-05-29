df1a99a7cab75d86537eeca0b4eb328082c7be39
Dihep <dihep.demayo@gmail.com>
2024-05-29 23:54:41 +0200

    M1- Sezione contenuti abbozzata.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,14 +19,23 @@ export default {
 
 
 <template>
-    <div id="vMain">{{ devAnswer }}</div>
+    <div id="mainContainer">
+        <h1>--> Content goes here <--</h1>
+    </div>
+    
 </template>
 
 
 
 <style scoped>
-#vMain {
+#mainContainer {
     height: 20vh;
-    border: 1px dashed blue;
+    background-color: rgb(30, 30, 30);
+    color: white;
+    padding: 0 2rem;
+}
+h1{
+    position: relative;
+    top: 35%;
 }
 </style>

f75176c4f07ca5a821298a882757bc48c51ca999
Dihep <dihep.demayo@gmail.com>
2024-05-29 23:48:35 +0200

    M1- Header completo.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,14 +19,46 @@ export default {
 
 
 <template>
-    <div id="vHeader">{{ devAnswer }}</div>
+    <nav>
+        <div id="navLogo"><img src="../assets/dc-logo.png" alt=""></div>
+        <ul>
+            <li>CHARACHTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </nav>
 </template>
 
 
 
 <style scoped>
-#vHeader {
-    height: 20vh;
-    border: 1px dashed red;
+nav {
+    height: 10vh;
+    padding: 0 15vw;
+    margin: 0 auto;
+    background-color: white;
+    color: rgb(30, 30, 30);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+#navLogo {
+    width: 30%;
+}
+ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+    width: 70%;
+}
+img {
+    height: 8vh;
 }
 </style>
